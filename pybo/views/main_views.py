d6313a350beda8ad42b4afd38c435a057835673b
kms12191 <ste_reo@naver.com>
2026-04-01 17:39:03 +0900

    260401

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,15 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, redirect, url_for
+
+from pybo.models import Question
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/')
 def index():
-    return 'Pybo index!'
+    return redirect(url_for('question._list'))
 
-@bp.route('/hello')
-def hello_world():
-    return 'Hello Pybo!'
-
-@bp.route('/bye')
-def bye_world():
-    return 'Bye Pybo!'
